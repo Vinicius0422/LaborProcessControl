8fadef5393175267c234b92934337441f995eee9
Vinicius0422 <guedescosta123@hotmail.com>
2023-01-31 08:51:57 -0300

    Primeiro commit

---
+++ src/test/java/br/com/vinicius/LaborProcessControl/LaborProcessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.LaborProcessControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborProcessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
